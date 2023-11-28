4785b1ee0cfc199bb0e8020ff44d3d1a8cbff054
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-11-28 04:57:56 -0800

    a

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,4 @@
-!/usr/bin/python3 
-for c in range(ord('a'), ord('z') + 1): 
-        if c != ord('e') and c != ord('q'):
-            print("{:c}".format(c), end="")
-                #!/usr/bin/python3
-# Author - Godswill Kalu
+#!/usr/bin/python3
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
