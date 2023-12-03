6496112f46cdccfe2c38e75013d38e16f8f6ab4b
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-12-03 05:17:37 -0800

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
-
-def element_at(my_list, idx):
-    c = add(a, b)
-    for i in range(4, 6):
-        c = add(c, i)
-        return (c)
-    else:
-        return (sub(a, b))
+def print_list_integer(my_list=[]):
+    for number in my_list:
+        print("{:d}".format(number))
