a8d35a6551465409040a290d5e3b83f021a89f1f
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-12-03 05:06:16 -0800

    0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    c = add(a, b)
+    for i in range(4, 6):
+        c = add(c, i)
+        return (c)
+    else:
+        return (sub(a, b))
