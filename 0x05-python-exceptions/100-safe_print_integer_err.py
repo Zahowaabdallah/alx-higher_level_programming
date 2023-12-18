8ec34ff44ba7a71080320f0333019767a5cca405
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-12-18 04:53:22 -0800

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,1 +1,11 @@
-l
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError)as error:
+        sys.stderr.write("Exception: {}\n".format(error))
+        return False
