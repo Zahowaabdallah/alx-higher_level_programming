431861fe2c8094532756b29157c646c810b12364
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2024-03-11 17:23:57 -0700

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-
-const process = require('process');
-const args = process.argv;
-
-if (isNaN(args[2]) || isNaN(args[3])) {
+if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const array = args.map(Number);
-  array.slice(2, args.length);
-  array.sort((a, b) => a - b);
-  console.log(array[array.length - 2]);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
